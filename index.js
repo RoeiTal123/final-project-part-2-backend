dcc1323799331faae668dfbea50b88d854b62cf4
Roei <Roeit1977@gmail.com>
2026-06-17 16:26:15 +0300

    downloaded cors so it can allow reading from the url

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express")
 const app = express()
 const fs = require("fs")
 const path = require("path")
+const cors = require("cors");
+
 app.use(express.json())
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}));
 
 app.get("/",(req,res)=>{
     res.send(JSON.stringify(req.query))
@@ -23,6 +30,22 @@ app.get("/users", (req, res) => {
     res.json(users)
 })
 
+app.get("/users/:id", (req, res) => {
+    const userId = req.params.id
+
+    const filePath = path.join(__dirname, "data", "users.json")
+    const data = fs.readFileSync(filePath, "utf-8")
+    const users = JSON.parse(data)
+    
+    const user = users.find(p => p._id === userId)
+
+    if (!user) {
+        return res.status(404).json({ error: "user not found" })
+    }
+
+    res.json(user)
+})
+
 app.post("/users", (req, res) => {
     console.log(req.body)
     res.json({ message: "received", data: req.body })
@@ -37,6 +60,22 @@ app.get("/posts", (req, res) => {
     const posts = JSON.parse(data)
 
     res.json(posts)
+})
+
+app.get("/posts/:id", (req, res) => {
+    const postId = req.params.id
+
+    const filePath = path.join(__dirname, "data", "posts.json")
+    const data = fs.readFileSync(filePath, "utf-8")
+    const posts = JSON.parse(data)
+
+    const post = posts.find(p => p._id === postId)
+
+    if (!post) {
+        return res.status(404).json({ error: "Post not found" })
+    }
+
+    res.json(post)
 })
 
 app.post("/posts", (req, res) => {
@@ -55,6 +94,22 @@ app.get("/communities", (req, res) => {
     res.json(communities)
 })
 
+app.get("/communitys/:id", (req, res) => {
+    const communityId = req.params.id
+
+    const filePath = path.join(__dirname, "data", "communitys.json")
+    const data = fs.readFileSync(filePath, "utf-8")
+    const communitys = JSON.parse(data)
+    
+    const community = communitys.find(p => p._id === communityId)
+
+    if (!community) {
+        return res.status(404).json({ error: "community not found" })
+    }
+
+    res.json(community)
+})
+
 app.post("/communities", (req, res) => {
     console.log(req.body)
     res.json({ message: "received", data: req.body })
@@ -69,6 +124,22 @@ app.get("/chats", (req, res) => {
     const chats = JSON.parse(data)
 
     res.json(chats)
+})
+
+app.get("/chats/:id", (req, res) => {
+    const chatId = req.params.id
+
+    const filePath = path.join(__dirname, "data", "chats.json")
+    const data = fs.readFileSync(filePath, "utf-8")
+    const chats = JSON.parse(data)
+    
+    const chat = chats.find(p => p._id === chatId)
+
+    if (!chat) {
+        return res.status(404).json({ error: "chat not found" })
+    }
+
+    res.json(chat)
 })
 
 app.post("/chats", (req, res) => {
